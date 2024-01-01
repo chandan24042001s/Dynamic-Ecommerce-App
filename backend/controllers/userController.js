4d5a5fc423cbbd7f164a9f0569930e19017a3561
Chandan <hb7102849@gmail.com>
2024-01-01 15:09:48 +0530

    login user successful

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,72 +1,107 @@
-const asyncHandler=require("express-async-handler")
-const jwt=require("jsonwebtoken")
-const bcrypt=require("bcryptjs")
-const generateToken=(id)=>{
-    return jwt.sign({id},process.env.JWT_SECRET,{
-        expiresIn: "1d"
-    })
-}
+const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const User = require("../models/userModel");
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+};
 
 //register user controller
-const registerUser=asyncHandler(async(req,res)=>{
-    const {name,email,password}=req.body;
-    //validation
-    if(!name || !email || !password){
-        res.status(400)
-        throw new Error("Please fill in all required fields")
-    }
-    if(password.length<6){
-        res.status(400)
-        throw new Error("password must be upto 6 character")
-    }
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  //validation
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please fill in all required fields");
+  }
+  if (password.length < 6) {
+    res.status(400);
+    throw new Error("password must be upto 6 character");
+  }
 
-    const User=require("../models/userModel")
-    //check if user exists
-    const userExists=await User.findOne({email})
-    if(userExists){
-        res.status(400)
-        throw new Error("Email already Registered")
-    };
+  const User = require("../models/userModel");
+  //check if user exists
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("Email already Registered");
+  }
 
-    //Create new User
-    const user=await User.create({
-        name,email,password
-    })
-    
+  //Create new User
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
 
-    //Generate Token for User
-    if(user){
-        const {_id,name,email,role}=User
-        const token=generateToken(_id)
-        res.cookie("token",token,{
-            path:"/",
-            httpOnly:true,
-            expires:new Date(Date.now()+1000*86400),
-            // secure:true,  for deployment
-            // sameSite:none
-        })
-         // send user data 
-        res.status(201).json({
-            success:true,
-            data:_id,name,email,role,token,
-            message:'regisered successfully'
-        })
-    }else{
-        res.status(400)
-        throw new Error("Invalid user Data")
-    }
-   
-  
-})
-
+  //Generate Token for User
+  if (user) {
+    const { _id, name, email, role } = User;
+    const token = generateToken(_id);
+    res.cookie("token", token, {
+      path: "/",
+      httpOnly: true,
+      expires: new Date(Date.now() + 1000 * 86400),
+      // secure:true,  for deployment
+      // sameSite:none
+    });
+    // send user data
+    res.status(201).json({
+      success: true,
+      data: _id,
+      name,
+      email,
+      role,
+      token,
+      message: "regisered successfully",
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user Data");
+  }
+});
 
 //Login User
-const loginUser=asyncHandler(async(req,res)=>{
-    res.send("login user.......")
-})
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  //validate Request
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("please add email & password");
+  }
 
+  //check if user Exists
+  const user = await User.findOne({ email });
+  //userexists, check if password is correct
+  const passwordIsCorrect = await bcrypt.compare(password, user.password);
 
-module.exports={
-    registerUser,
-    loginUser
-}
+  if(!user){
+    res.status(400)
+    throw new Error("user doesnot exist")
+  }
+
+  //generate token for new user
+  const token = generateToken(user._id);
+  if (user && passwordIsCorrect) {
+    const newUser=await User.findOne({email}).select("-password")
+    res.cookie("token",token,{
+        path:"/",
+        httpOnly:true,
+        expires: new Date(Date.now()+1000*86400),
+        // secure:true,
+        // sameSite:none,
+    });
+    //send user data
+    res.status(201).json(newUser)
+  }else{
+    res.status(400);
+    throw new Error("Invalid email or Password");
+  }
+});
+
+module.exports = {
+  registerUser,
+  loginUser,
+};
